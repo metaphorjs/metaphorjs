9ba2e269840657f0db3f89583827213de5bbf386
Ivan Kuindzhi <kuindji@gmail.com>
2018-08-06 18:29:46 +0300

    Include directive: +asis mode

--- src/directive/attr/include.js
+++ src/directive/attr/include.js
@@ -1,26 +1,23 @@
 
 
 var Directive = require("../../class/Directive.js"),
+    toBool = require("../../func/toBool.js"),
     Template = require("../../class/Template.js");
 
 Directive.registerAttribute("include", 1100,
     function(scope, node, tplExpr, parentRenderer, attr){
 
-    var cfg = attr ? attr.config : {};
+    var cfg = attr ? attr.config : {},
+        asis = toBool(cfg.asis);
 
     var tpl = new Template({
         scope: scope,
         node: node,
         url: tplExpr,
         parentRenderer: parentRenderer,
-        animate: !!cfg.animate
+        animate: !!cfg.animate,
+        ownRenderer: !asis // do not render if asis=true
     });
 
     return false; // stop renderer
-    /*if (tpl.ownRenderer) {
-        return false;
-    }
-    else {
-        return tpl.initPromise;
-    }*/
 });
--- src/extra-directives/tag/include.js
+++ src/extra-directives/tag/include.js
@@ -1,21 +1,22 @@
 
 var Directive = require("../../class/Directive.js"),
     getAttr = require("../../func/dom/getAttr.js"),
+    toBool = require("../../func/toBool.js"),
     Template = require("../../class/Template.js");
 
-Directive.registerTag("include", function(scope, node, value, parentRenderer) {
+Directive.registerTag("include", function(scope, node, value, parentRenderer, attr) {
+
+    var cfg = (attr ? attr.config : {}) || {},
+        asis = toBool(cfg.asis);
 
     var tpl = new Template({
         scope: scope,
         node: node,
         url: getAttr(node, "src"),
         parentRenderer: parentRenderer,
-        replace: true
+        replace: true,
+        ownRenderer: !asis // if asis, do not render stuff
     });
 
-
-    //return tpl.initPromise;
-
-    return false;
-
+    return false; // stop renderer
 });
