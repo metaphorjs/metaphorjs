ba4a9dbd8ca4de3d6f8c0fa0487fc3f18f4d6011
Johann Kuindji <kuindji@gmail.com>
2019-01-02 19:16:54 +0300

    in-focus: component support

--- src/directive/attr/in-focus.js
+++ src/directive/attr/in-focus.js
@@ -7,6 +7,13 @@ Directive.registerAttribute("in-focus", 500, Directive.$extend({
 
     $class: "MetaphorJs.app.Directive.attr.InFocus",
 
+    $init: function(scope, node) {
+        if (node.getDomApi) {
+            arguments[1] = node.getDomApi();
+        }
+        this.$super.apply(this, arguments);
+    },
+
     initialSet: function() {
         this.config.setType("value", "bool");
         this.$super();
