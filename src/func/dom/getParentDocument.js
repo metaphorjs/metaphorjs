8c475164ec02e80d9921623822fb8fdae5601851
Johann Kuindji <kuindji@gmail.com>
2019-03-04 15:35:44 +0300

    + MetaphorJs.dom.getParentDocument

---
+++ src/func/dom/getParentDocument.js
@@ -0,0 +1,16 @@
+
+require("./__init.js");
+
+var MetaphorJs = require("metaphorjs-shared/src/MetaphorJs.js");
+
+module.exports = MetaphorJs.dom.getParentDocument = function dom_getParentDocument(node) {
+    var fragType = window.document.DOCUMENT_FRAGMENT_NODE,
+        parent = node.parentNode;
+    while (parent) {
+        if (parent.nodeType === fragType) {
+            return parent;
+        }
+        parent = parent.parentNode;
+    }
+    return node.ownerDocument;
+};
